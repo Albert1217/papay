4fb7d1d26917c4412eae5d043ebf20cc302111e1
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-05-30 05:52:21 +0900

    feat: change some version

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const express = require("express");
 const app = express();
 const router = require("./ router");
 
-// MongoDB choqirish
-const db = require("./server").db();
-const mongodb = require("mongodb");
-
 // 1 Kirish code
 app.use(express.static("public"));
 app.use(express.json());
--- server.js
+++ server.js
@@ -2,21 +2,20 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const http = require("http");
-const mongodb = require("mongodb");
+const mongoose = require("mongoose");
 
 const connectionString = process.env.MONGO_URL;
-mongodb.connect(
+mongoose.connect(
   connectionString,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
-  (err, client) => {
+  (err, goose) => {
     if (err) console.log("ERROR on connection MongoDB");
     else {
       console.log("MongoDB connection succeedd");
-      module.exports = client;
-
+      console.log(goose);
       const app = require("./app");
       const server = http.createServer(app);
       let PORT = process.env.PORT || 3011;
