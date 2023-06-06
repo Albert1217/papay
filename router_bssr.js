b0633ad85d7efbb5027e9b05831a9116499fdd5f
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-06-06 18:02:04 +0900

    feat: multer single image upload process

--- router_bssr.js
+++ router_bssr.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router_bssr = express.Router();
 const restaurantController = require("./controllers/restaurantController");
 const productController = require("./controllers/productController");
+const { uploadsProductImage } = require("./utils/upload-multer");
 
 /***********************
  *      BSSR EJS       *
@@ -21,6 +22,7 @@ router_bssr.get("/products/menu", restaurantController.getMyRestaurantData);
 router_bssr.post(
   "/products/create",
   restaurantController.validateAuthRestaurant,
+  uploadsProductImage.single("product_image"),
   productController.addNewProduct
 );
 router_bssr.post("/products/edit/:id", productController.updateChosenProduct);
