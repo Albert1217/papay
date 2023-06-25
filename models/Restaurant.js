96e4a6b01f24e05f9610c69fd8360ecdde68ff32
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-06-25 12:08:09 +0900

    feat: complate development of allrestaurants business logic

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,6 +1,7 @@
 const assert = require("assert");
 const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/mistake");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
 
 class Restaurant {
   constructor() {
@@ -15,6 +16,24 @@ class Restaurant {
         .exec();
 
       assert(result, Definer.general_err1);
+      return result;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async updateRestaurantByAdminData(update_data) {
+    try {
+      const id = shapeIntoMongooseObjectId(update_data?.id);
+      const result = await this.memberModel
+        .findByIdAndUpdate({ _id: id }, update_data, {
+          runValidators: true,
+          lean: true,
+          returnDocument: "after",
+        })
+        .exec();
+
+      assert.ok(result, Definer.general_err1);
       return result;
     } catch (err) {
       throw err;
--- router_bssr.js
+++ router_bssr.js
@@ -44,4 +44,10 @@ router_bssr.get(
   restaurantController.getAllRestaurants
 );
 
+router_bssr.post(
+  "/all-restaurant/edit",
+  restaurantController.validateAdmin,
+  restaurantController.updateRestaurantByAdmin
+);
+
 module.exports = router_bssr;
