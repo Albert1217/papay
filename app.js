c92f9caedd2f62737e0c7b0f032c26902ac63e27
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-07-17 16:10:27 +0900

    feat: create jwt tokens and enable cookie-parser

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 const router_bssr = require("./router_bssr.js");
 const router = require("./router.js");
+const cookieParser = require("cookie-parser");
 
 let session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
@@ -17,6 +18,7 @@ const store = new MongoDBStore({
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // 2: Session code
 app.use(
--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,51 +1,72 @@
 const Member = require("../models/Member");
+const jwt = require("jsonwebtoken");
+const assert = require("assert");
+const Definer = require("../lib/mistake");
 
 let memberController = module.exports;
 
 memberController.signup = async (req, res) => {
   try {
-    console.log("POST: cont/signup");
+    console.log("POST:cont/signup");
     const data = req.body;
 
     const member = new Member();
-
     const new_member = await member.signupData(data);
 
-    //TODO: AUTHENTICATE BASED ON JWT
+    const token = memberController.createToken(new_member);
+
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: false,
+    });
 
     res.json({ state: "success", data: new_member });
   } catch (err) {
-    console.log(`ERROR,cont/signup, ${err.message}`);
+    console.log(`ERROR, cont/signup, ${err.message}`);
     res.json({ state: "fail", message: err.message });
-    // if (err.message === "mb_nick is already in use") {
-    //   res
-    //     .status(400)
-    //     .json({ state: "fail", message: "this member is already in use" });
-    // }
   }
 };
 
 memberController.login = async (req, res) => {
   try {
     console.log("POST: cont/login");
-    const data = req.body;
-    const member = new Member();
-    const new_member = await member.loginData(data);
+    const data = req.body,
+      member = new Member(),
+      result = await member.loginData(data);
 
-    res.json({ state: "success", data: new_member });
+    const token = memberController.createToken(result);
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
+
+    res.json({ state: "success", data: result });
   } catch (err) {
-    console.log(`ERROR,cont/login ${err.message}`);
+    console.log(`ERROR, cont/login, ${err.message}`);
     res.json({ state: "fail", message: err.message });
-
-    // if (err.message === "mb_nick is already in use") {
-    //   res
-    //     .status(400)
-    //     .json({ state: "fail", message: "this member is already in use" });
-    // }
   }
 };
 
 memberController.logout = (req, res) => {
   console.log("GET cont.logout");
   res.send("logout  sahifadasiz");
+};
+
+memberController.createToken = (result) => {
+  try {
+    const upload_data = {
+      _id: result._id,
+      mb_nick: result.mb_nick,
+      mb_type: result.mb_type,
+    };
+
+    const token = jwt.sign(upload_data, process.env.SECRET_TOKEN, {
+      expiresIn: "6h",
+    });
+
+    assert.ok(token, Definer.auth_err2);
+    return token;
+  } catch (err) {
+    throw err;
+  }
 };
