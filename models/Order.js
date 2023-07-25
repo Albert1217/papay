e982221ad0aeddedc49e8b5e494524c354dfbedc
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-07-25 21:22:33 +0900

    feat: fulfil  development of  order & orderItems api business logic

--- models/Order.js
+++ models/Order.js
@@ -1,3 +1,5 @@
+const assert = require("assert");
+const { shapeIntoMongooseObjectId } = require("../lib/config");
 const Definer = require("../lib/mistake");
 const OrderModel = require("../schema/order.model");
 const OrderItemModel = require("../schema/order_item.model");
@@ -29,7 +31,7 @@ class Order {
 
       console.log("order_id:::", order_id);
 
-      //TODO: order items creation
+      await this.recordOrderItemData(order_id, data);
 
       return order_id;
     } catch (err) {
@@ -50,6 +52,40 @@ class Order {
     } catch (err) {
       console.log(err);
       throw new Error(Definer.order_err1);
+    }
+  }
+
+  async recordOrderItemData(order_id, data) {
+    try {
+      const pro_list = data.map(async (item) => {
+        return await this.saveOrderItemData(item, order_id);
+      });
+
+      const results = await Promise.all(pro_list);
+      console.log("result::::", results);
+      return true;
+    } catch (err) {
+      throw err;
+    }
+  }
+
+  async saveOrderItemData(item, order_id) {
+    try {
+      order_id = shapeIntoMongooseObjectId(order_id);
+      item._id = shapeIntoMongooseObjectId(item._id);
+
+      const order_item = new this.orderItemModel({
+        item_quantity: item["quantity"],
+        item_price: item["price"],
+        order_id: order_id,
+        product_id: item["_id"],
+      });
+      const result = await order_item.save();
+      assert.ok(result, Definer.order_err2);
+      return "created";
+    } catch (err) {
+      console.log(err);
+      throw new Error(Definer.order_err2);
     }
   }
 }
