0601ab61828f9483e964465fc2737ee22d15ce6a
Albert1217 <ismoiljonmuxtorov880@gmail.com>
2023-07-28 06:13:58 +0900

    fix: modify getMemberArticles api

--- models/Community.js
+++ models/Community.js
@@ -49,6 +49,7 @@ class Community {
               as: "member_data",
             },
           },
+          { $unwind: "$member_data" },
         ])
         .exec();
       assert.ok(result, Definer.article_err2);
